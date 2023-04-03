2b5625c54002a17e8bfacac1634e9ef2eb6ec7fa
Crowdbotics <team@crowdbotics.com>
2023-04-03 02:25:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vasco_bxh_39671/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vasco_bxh_39671;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vasco_bxh_39671";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vasco-bxh-39671",
+  url: "https://vasco-bxh-39671.botics.co",
+  api: "https://vasco-bxh-39671.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
